e05f759580f08137125096ead80d66603c0e39ee
Igodlab <igodlab@gmail.com>
2023-05-27 16:12:58 +0000

    Playing with transactions and Gift SC

--- code/Week02/scripts/collect-gift.sh
+++ code/Week02/scripts/collect-gift.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-assets=/workspace/code/Week02/assets
-keypath=/workspace/keys
+assets=$HOME/workspace/repo/code/Week02/assets
+keypath=$HOME/workspace/repo/keys
 name="$1"
 collateral="$2"
 txin="$3"
--- code/Week02/scripts/make-gift.sh
+++ code/Week02/scripts/make-gift.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-assets=/workspace/code/Week02/assets
-keypath=/workspace/keys
+assets=$HOME/workspace/repo/code/Week02/assets
+keypath=$HOME/workspace/repo/keys
 name="$1"
 txin="$2"
 body="$assets/gift.txbody"
